8c0ad22e49e0c03446c4db02475edcdc189029b9
Tareq <tareq1obd@gmail.com>
2024-12-26 19:14:51 +0600

    Updated

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAU8zeogC0GMlO3Inng7X73ESc88l7G390",
+  authDomain: "royal-homes-9be62.firebaseapp.com",
+  projectId: "royal-homes-9be62",
+  storageBucket: "royal-homes-9be62.appspot.com",
+  messagingSenderId: "962597563982",
+  appId: "1:962597563982:web:ea6a7cd13b596e03981d1f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
